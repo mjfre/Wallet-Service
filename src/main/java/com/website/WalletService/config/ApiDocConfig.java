126690ae26e52649f96f9c9a3dd0634f1293dc4b
mjfre <mjfre@aol.com>
2022-01-19 18:49:06 -0800

    adding config files

---
+++ src/main/java/com/website/WalletService/config/ApiDocConfig.java
@@ -0,0 +1,51 @@
+package com.website.WalletService.config;
+
+import com.google.common.collect.Lists;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class ApiDocConfig {
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("Matt Freedman", "https://www.jointheleague.org", "mjfre@aol.com");
+        return new ApiInfoBuilder()
+                .title("Wallet Servicee")
+                .description("Spring Boot API that stores and distributes Thor wallet addresses, given a Terra wallet address")
+                .contact(contact)
+                .build();
+    }
+
+    @Bean
+    public Docket api() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        List<SecurityContext> contextList = new ArrayList<>();
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .produces(Collections.singleton("application/json"))
+                .consumes(Collections.singleton("application/json"))
+                .securitySchemes(schemeList)
+                .securityContexts(contextList)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.jointheleague.api.survey.presentation"))
+                .paths(regex("/.*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+}
---
+++ src/main/java/com/website/WalletService/exception/ApiExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.website.WalletService.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {ApiRequestException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+        //Create payload to send inside response entity
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                badRequest,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        //Return the response entity
+        return new ResponseEntity<>(apiException, badRequest);
+    }
+
+}
